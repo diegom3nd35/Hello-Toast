217c527c88b48e8e1a2d288c3eaf1b4e61f7997d
diegom3nd35 <diegom3nd35@gmail.com>
2018-12-14 18:31:14 -0300

    Adicionando msg no toast

--- HelloToast/app/src/main/java/com/example/diego/hellotoast/app/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/diego/hellotoast/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.diego.hellotoast.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.diego.hellotoast.R;
 
@@ -13,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Hello Toast!
+    public void toast (View view){
+        Toast.makeText(getApplicationContext(),"Hello Toast!",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    public void contar (View view){
+
+
+    }
 
 }
